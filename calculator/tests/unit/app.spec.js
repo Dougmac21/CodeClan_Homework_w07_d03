acaa725717a43744b673d6ca010f0ac65b52ad30
Douglas MacIntyre <dougmac21@talk21.com>
2020-10-21 20:03:31 +0100

    Add test passes.

--- calculator/tests/unit/app.spec.js
+++ calculator/tests/unit/app.spec.js
@@ -1,5 +1,6 @@
 import App from "@/App.vue";
 import { shallowMount } from '@vue/test-utils';
+import { expect } from "chai";
 
 
 describe('App', () => {
@@ -9,30 +10,32 @@ describe('App', () => {
     });
 
     it('should be able to add two numbers', () => {
+        wrapper.vm.previousTotal = 4;
+        wrapper.vm.add(1);
+        expect(wrapper.vm.runningTotal).to.equal(5);
+    });
+
+    xit('should be able to subtract a number from a number', () => {
 
     });
 
-    it('should be able to subtract a number from a number', () => {
+    xit('should be able to multiply one number by another', () => {
 
     });
 
-    it('should be able to multiply one number by another', () => {
+    xit('should be able to divide one number by another', () => {
 
     });
 
-    it('should be able to divide one number by another', () => {
+    xit('should concatinate multiple clicks of a number button', () => {
 
     });
 
-    it('should concatinate multiple clicks of a number button', () => {
+    xit('should chain multiple operations together', () => {
 
     });
 
-    it('should chain multiple operations together', () => {
-
-    });
-
-    it('should clear the running total without any impact on the calculation', () => {
+    xit('should clear the running total without any impact on the calculation', () => {
 
     });
     
