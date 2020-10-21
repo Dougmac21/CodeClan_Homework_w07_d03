937a1e2fbb202dcb9d095ea5860cde8a8b7d5b23
Douglas MacIntyre <dougmac21@talk21.com>
2020-10-21 19:45:36 +0100

    Wrote Unit Test Descriptions.

---
+++ calculator/tests/unit/app.spec.js
@@ -0,0 +1,41 @@
+import App from "@/App.vue";
+import { shallowMount } from '@vue/test-utils';
+
+
+describe('App', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallowMount(App);
+    });
+
+    it('should be able to add two numbers', () => {
+
+    });
+
+    it('should be able to subtract a number from a number', () => {
+
+    });
+
+    it('should be able to multiply one number by another', () => {
+
+    });
+
+    it('should be able to divide one number by another', () => {
+
+    });
+
+    it('should concatinate multiple clicks of a number button', () => {
+
+    });
+
+    it('should chain multiple operations together', () => {
+
+    });
+
+    it('should clear the running total without any impact on the calculation', () => {
+
+    });
+    
+});
+
+
