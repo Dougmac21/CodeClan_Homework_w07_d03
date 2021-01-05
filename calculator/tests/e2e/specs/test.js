9a809c0bb6f6044dcf9a1bd5a5c978fc60c2b7d8
Douglas MacIntyre <dougmac21@talk21.com>
2021-01-05 12:57:01 +0000

    Corrections to remove skip on non-divide by zero tests and grey-out zero by N test which is not needed

--- calculator/tests/e2e/specs/test.js
+++ calculator/tests/e2e/specs/test.js
@@ -5,12 +5,12 @@ describe('calculator', () => {
     cy.visit('/')
   });
 
-  xit('should have working number buttons', () => {
+  it('should have working number buttons', () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   });
 
-  xit('should have number button presses update the running total', () => {
+  it('should have number button presses update the running total', () => {
     cy.get('#number1').click();
     cy.get('#number2').click();
     cy.get('#number3').click();
@@ -18,7 +18,7 @@ describe('calculator', () => {
     cy.get('.display').should('contain', '1234');
   });
 
-  xit('should update the display with the result of an operation', () => {
+  it('should update the display with the result of an operation', () => {
     cy.get('#number1').click();
     cy.get('#number0').click();
     cy.get('#operator_add').click();
@@ -28,7 +28,7 @@ describe('calculator', () => {
     cy.get('.display').should('contain', '20');
   });
 
-  xit('should chain multiple operations correctly', () => {
+  it('should chain multiple operations correctly', () => {
     cy.get('#number1').click();
     cy.get('#number0').click();
     cy.get('#number0').click();
@@ -42,7 +42,7 @@ describe('calculator', () => {
     cy.get('.display').should('contain', '800');
   });
 
-  xit('should output positive numbers as normal', () => {
+  it('should output positive numbers as normal', () => {
     cy.get('#number3').click();
     cy.get('#number3').click();
     cy.get('#operator_subtract').click();
@@ -52,7 +52,7 @@ describe('calculator', () => {
     cy.get('.display').should('contain', '23');
   });
 
-  xit('should output negative numbers with an indictor that they are negative', () => {
+  it('should output negative numbers with an indictor that they are negative', () => {
     cy.get('#number2').click();
     cy.get('#number5').click();
     cy.get('#operator_subtract').click();
@@ -62,7 +62,7 @@ describe('calculator', () => {
     cy.get('.display').should('contain', '-50');
   });
 
-  xit('should output non-whole numbers as a decimal up to the display size limit', () => {
+  it('should output non-whole numbers as a decimal up to the display size limit', () => {
     cy.get('#number2').click();
     cy.get('#number2').click();
     cy.get('#operator_divide').click();
@@ -71,7 +71,7 @@ describe('calculator', () => {
     cy.get('.display').should('contain', '3.142857142857143');
   });
 
-  xit('should output numbers beyond the display space of the output view as an exponent', () => {
+  it('should output numbers beyond the display space of the output view as an exponent', () => {
     cy.get('#number1').click();
     cy.get('#number0').click();
     cy.get('#number0').click();
@@ -117,11 +117,11 @@ describe('calculator', () => {
     cy.get('.display').should('contain', 'Divide By Zero Error');
   });
 
-  xit('should return zero for zero divided by a non-zero number', () => {
-    cy.get('#number0').click();
-    cy.get('#operator_divide').click();
-    cy.get('#number8').click();
-    cy.get('#operator_equals').click();
-    cy.get('.display').should('contain', '0');
-  });
+  // xit('should return zero for zero divided by a non-zero number', () => {
+  //   cy.get('#number0').click();
+  //   cy.get('#operator_divide').click();
+  //   cy.get('#number8').click();
+  //   cy.get('#operator_equals').click();
+  //   cy.get('.display').should('contain', '0');
+  // });
 });
